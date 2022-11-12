60258958d05f9718a38f21d9636d6e131c9bbf9c
Albert Zhang <32472855+alzh9000@users.noreply.github.com>
2022-11-12 12:11:03 -0500

    hackathon starting

--- src/Game.js
+++ src/Game.js
@@ -2,39 +2,36 @@ import React from 'react';
 import { Table } from 'react-bootstrap';
 
 const Game = (parsedGames) => {
-    parsedGames = parsedGames["parsedGames"];
+	parsedGames = parsedGames['parsedGames'];
 
-    function singleGame(oneGame) {
-        return (
-            <tr>
-                <td>{oneGame[0]}</td>
-                <td>{oneGame[1]}</td>
-                <td>{oneGame[2]}</td>
-                <td>{oneGame[3]}</td>
-                <td>{oneGame[4]}</td>
-            </tr>
-        );
+	function singleGame(oneGame) {
+		return (
+			<tr>
+				<td>{oneGame[0]}</td>
+				<td>{oneGame[1]}</td>
+				<td>{oneGame[2]}</td>
+				<td>{oneGame[3]}</td>
+				<td>{oneGame[4]}</td>
+			</tr>
+		);
+	}
 
-    };
-
-    return (
-        <div id="table-container">
-            <Table bordered hover variant="dark">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Away Team</th>
-                        <th>Home Team</th>
-                        <th>Date</th>
-                        <th>Game Status</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {parsedGames.map(singleGame)}
-                </tbody>
-            </Table>
-        </div>
-    );
+	return (
+		<div id='table-container'>
+			<Table bordered>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Away Team</th>
+						<th>Home Team</th>
+						<th>Date</th>
+						<th>Game Status</th>
+					</tr>
+				</thead>
+				<tbody>{parsedGames.map(singleGame)}</tbody>
+			</Table>
+		</div>
+	);
 };
 
 export default Game;
--- src/Landing.js
+++ src/Landing.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import HarmonyBasketball from './utils/HarmonyBasketball.mp4'
-import HarmonyBasketballLogo from './utils/BasketballLogoLight.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import HarmonyBasketball from './utils/tron_hype.mp4';
+import HarmonyBasketballLogo from './utils/tron_bet_high_resolution.png';
 import './App.css';
 
 const Landing = () => {
-    return (
-        <div className="App">
-            <video id='backgroundVideo' autoPlay loop muted>
-                <source src={HarmonyBasketball} type='video/mp4' />
-            </video>
-            <div id="splash-items">
-                <img src={HarmonyBasketballLogo} alt="logo" width="30%" />
-                <h1 id="title-splash">Blockchain Basketball Betting</h1>
-                <br></br>
-                <button id="get-started-button">
-                    <Link id="launch-link" to="/app">Launch the App</Link>
-                </button>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className='App'>
+			<video id='backgroundVideo' autoPlay loop muted>
+				<source src={HarmonyBasketball} type='video/mp4' />
+			</video>
+			<div id='splash-items'>
+				<img src={HarmonyBasketballLogo} alt='logo' width='50%' />
+				<h1 id='title-splash'>Tron Bet</h1>
+				<br></br>
+				<button id='get-started-button'>
+					<Link id='launch-link' to='/app'>
+						Launch the App
+					</Link>
+				</button>
+			</div>
+		</div>
+	);
+};
 
-export default Landing
+export default Landing;
